efe959b7ab3b6febe9ba41b825eeaf6ecd716846
Beau Dobbin <beau@beaunus.com>
2020-07-18 18:40:13 +0900

    refactor: rename .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: { es6: true, node: true },
+  extends: [
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: 2020 },
+  plugins: ["@typescript-eslint", "import"],
+  root: true,
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "no-shadow": "warn",
+    "sort-keys": "warn",
+    "no-unused-vars": "error",
+    "import/order": [
+      "error",
+      {
+        alphabetize: { order: "asc", caseInsensitive: true },
+        groups: ["builtin", "external", "parent", "sibling", "index"],
+        "newlines-between": "always",
+      },
+    ],
+    yoda: "error",
+  },
+};
